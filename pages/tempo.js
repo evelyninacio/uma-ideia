9910acb13385cbe8e9c8f34b59e443753451657d
Evelyn Oliveira <evelynoliveira@MacBook-Air-de-Evelyn.local>
2025-12-04 13:11:17 -0300

    adicionando delay

--- pages/tempo.js
+++ pages/tempo.js
@@ -7,18 +7,21 @@ function Tempo({ staticDateString }) {
       <h1>Tempo Page</h1>
       {dynamicDateString}(dinamico)
         <br />
-      {staticDateString}(estatico)
+      {staticDateString}(estatico com delay)
     </div>
   );
 }
 
 export async function getStaticProps() {
     console.log("Passando pelo getStaticProps");
+    console.log("Delay de 5 segundos");
+
+    await delay(5000);
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
   return {
     props: { staticDateString },
   };
 }
-
+const delay = ms => new Promise(res => setTimeout(res, ms));
 export default Tempo;
